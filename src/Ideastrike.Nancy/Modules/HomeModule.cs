bac5546d61868db0a56b70225bffdd2d6ccb34ec
aeoth <paul@theleagueofpaul.com>
2012-01-17 08:13:16 +1100

    Nancy blank project added

---
+++ src/Ideastrike.Nancy/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Ideastrike.Nancy.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello, world";
+        }
+    }
+}
