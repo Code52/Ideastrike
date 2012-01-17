5df71d23e70c8297ce41e218d33f9fee9ce9bd94
Piers Karsenbarg <email@piersonthe.net>
2012-01-17 11:51:58 +0000

    Refactored model code into a repository. Updated Home and Idea modules to use this repository.

---
+++ src/Ideastrike.Nancy/Models/IdeaRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ideastrike.Nancy.Models
+{
+    public class IdeaRepository : IIdeaRepository
+    {
+        private IdeastrikeContext db = new IdeastrikeContext();
+
+        IEnumerable<Idea> IIdeaRepository.GetAllIdeas()
+        {
+            return (db.Ideas.ToList());
+        }
+
+        void IIdeaRepository.AddIdea(Idea idea)
+        {
+            db.Ideas.Add(idea);
+            db.SaveChanges();
+        }
+
+        void IIdeaRepository.DeleteIdea(double IdeaId)
+        {
+            double id = IdeaId;
+            var idea = db.Ideas.FirstOrDefault(i => i.Id == id);
+            db.Ideas.Remove(idea);
+            db.SaveChanges();
+           
+        }
+
+        Idea IIdeaRepository.GetIdea(double id)
+        {
+            return db.Ideas.FirstOrDefault(i => i.Id == id);
+        }                    
+
+        void IIdeaRepository.UpdateIdea(Idea idea)
+        {
+            var tmpIdea = db.Ideas.Single(i => i.Id == idea.Id);
+            tmpIdea = idea;
+            db.SaveChanges();
+        }
+
+        int IIdeaRepository.CountIdeas()
+        {
+            return db.Ideas.Count();
+        }
+
+        
+    }
+}
--- src/Ideastrike.Nancy/Modules/HomeModule.cs
+++ src/Ideastrike.Nancy/Modules/HomeModule.cs
@@ -7,16 +7,13 @@ namespace Ideastrike.Nancy.Modules
 {
     public class HomeModule : NancyModule
     {
+        IIdeaRepository IdeaRep = new IdeaRepository();
         public HomeModule()
         {
             Get["/"] = _ =>
             {
-                using (var db = new IdeastrikeContext())
-                {
-                    db.Ideas.Add(new Idea {Title = "I heard you like ideas, so I put an idea in your ideas", Time = DateTime.UtcNow});
-                    db.SaveChanges();
-                    return View["Home/Index", string.Format("Hello, world. There are {0} ideas", db.Ideas.Count())];
-                }
+                IdeaRep.AddIdea(new Idea { Title = "I heard you like ideas, so I put an idea in your ideas", Time = DateTime.UtcNow });
+                return View["Home/Index", string.Format("Hello, world. There are {0} ideas", IdeaRep.CountIdeas())];
             };
         }
     }
