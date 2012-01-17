382da1c8922ddf8ae8bbc998cc072bb9702a3906
Ryan Lowdermilk <ryanlowdermilk@gmail.com>
2012-01-16 21:54:39 -0800

    renamed Post to Comment
    added Models and Modules (create and delete)
    added View for Comment

--- src/Ideastrike.Nancy/Models/Idea.cs
+++ src/Ideastrike.Nancy/Models/Idea.cs
@@ -7,6 +7,7 @@ namespace Ideastrike.Nancy.Models
     public class Idea
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
