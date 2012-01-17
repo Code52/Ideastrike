c3df898623aa3b2a65cc9fd071f7a8fd8759aeba
aeoth <paul@theleagueofpaul.com>
2012-01-17 13:13:30 +1100

    Fixed up the base layout so it includes a renderbody, and an example view

--- src/Ideastrike.Nancy/Modules/HomeModule.cs
+++ src/Ideastrike.Nancy/Modules/HomeModule.cs
@@ -15,9 +15,8 @@ namespace Ideastrike.Nancy.Modules
                                {
                                    db.Ideas.Add(new Idea {Title = "I heard you like ideas, so I put an idea in your ideas", Time = DateTime.UtcNow});
                                    db.SaveChanges();
-                                   return string.Format("Hello, world. There are {0} ideas", db.Ideas.Count());    
+                                   return View["Home/Index", string.Format("Hello, world. There are {0} ideas", db.Ideas.Count())];
                                }
-                               
                            };
         }
     }
