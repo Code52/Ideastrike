12511cdad41682f8fc1dbaafc2fdbda7d4702842
Bryce Carman <bryce@greentab.net>
2012-01-21 19:51:05 -0800

    Fix compile warnings

--- src/Ideastrike.Nancy/Models/Comment.cs
+++ src/Ideastrike.Nancy/Models/Comment.cs
@@ -3,11 +3,6 @@ namespace Ideastrike.Nancy.Models
 {
     public class Comment : Activity
     {
-        [Key]
-        public int Id { get; set; }
-
-        public int IdeaId { get; set; }
-
         public string Text { get; set; }
 
         public int UserId { get; set; }
