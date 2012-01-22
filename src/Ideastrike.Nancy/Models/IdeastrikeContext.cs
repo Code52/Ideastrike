2eb3408eb18637a1c8d288c859520e9e93fd435f
Brendan Forster <me@brendanforster.com>
2012-01-22 18:00:42 +1100

    checking for specific connection string

--- src/Ideastrike.Nancy/Models/IdeastrikeContext.cs
+++ src/Ideastrike.Nancy/Models/IdeastrikeContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Configuration;
+using System.Data.Entity;
 
 namespace Ideastrike.Nancy.Models
 {
@@ -7,6 +8,13 @@ namespace Ideastrike.Nancy.Models
         public IdeastrikeContext()
         {
             Configuration.ProxyCreationEnabled = false;
+
+            var apphb = ConfigurationManager.ConnectionStrings["SQLSERVER_CONNECTION_STRING"];
+
+            if (apphb != null)
+            {
+                Database.Connection.ConnectionString = apphb.ConnectionString;
+            }
         }
 
         public DbSet<Idea> Ideas { get; set; }
