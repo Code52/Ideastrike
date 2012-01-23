f52feba2ccdb016ee21fbcedb7e33265983ac530
aeoth <paul@theleagueofpaul.com>
2012-01-23 17:44:17 +1100

    manual merge fail. oops

--- src/Ideastrike.Nancy/DevelopmentDatabaseInitializer.cs
+++ src/Ideastrike.Nancy/DevelopmentDatabaseInitializer.cs
@@ -12,30 +12,26 @@ namespace Ideastrike.Nancy
         protected override void Seed(IdeastrikeContext context)
         {
 
-            context.Settings.Add(new Setting {Key = "Title", Value = "Yet Another Ideastrike"});
-            context.Settings.Add(new Setting {Key = "Name", Value = "Ideastrike"});
-            context.Settings.Add(new Setting {Key = "WelcomeMessage", Value = "You've been.... Ideastruck"});
-            context.Settings.Add(new Setting {Key = "HomePage", Value = "http://www.code52.org"});
-            context.Settings.Add(new Setting {Key = "GAnalyticsKey", Value = ""});
+            context.Settings.Add(new Setting { Key = "Title", Value = "Yet Another Ideastrike" });
+            context.Settings.Add(new Setting { Key = "Name", Value = "Ideastrike" });
+            context.Settings.Add(new Setting { Key = "WelcomeMessage", Value = "You've been.... Ideastruck" });
+            context.Settings.Add(new Setting { Key = "HomePage", Value = "http://www.code52.org" });
+            context.Settings.Add(new Setting { Key = "GAnalyticsKey", Value = "" });
 
-            var status = new Status { Title = "New" };
-
-            context.Statuses.Add(status);
-            context.Statuses.Add(new Status { Title = "Active" });
-            context.Statuses.Add(new Status { Title = "Completed" });
-            context.Statuses.Add(new Status { Title = "Declined" });
+            string ideaStatusDefault = "New";
+            context.Settings.Add(new Setting { Key = "IdeaStatusChoices", Value = "New,Active,Completed,Declined" });
+            context.Settings.Add(new Setting { Key = "IdeaStatusDefault", Value = ideaStatusDefault });
 
             context.SaveChanges();
 
-            var firstIdea = new Idea
+            context.Ideas.Add(new Idea
                                   {
                                       Time = DateTime.UtcNow,
                                       Author = new User { Id = Guid.NewGuid(), UserName = "aeoth", Email = "paul@theleagueofpaul.com" },
                                       Title = "So Meta",
                                       Description = "Put an idea in your idea so you can idea when you idea",
-                                      Status = status
-                                  };
-            context.Ideas.Add(firstIdea);
+                                      Status = ideaStatusDefault
+                                  });
 
             context.Ideas.Add(new Idea
             {
@@ -43,8 +39,10 @@ namespace Ideastrike.Nancy
                 Author = new User { Id = Guid.NewGuid(), UserName = "shiftkey", Email = "me@brendanforster.com" },
                 Title = "Lorem Ipsum",
                 Description = "This is also another idea",
-                Status = status
+                Status = ideaStatusDefault
             });
+
+            // TODO: define some more data
 
             context.SaveChanges();
         }
