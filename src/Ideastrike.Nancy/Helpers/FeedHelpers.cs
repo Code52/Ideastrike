0bf0e27ed2dd9557f8448097045ad7f0d406cec9
Andrew Tobin <andrew@grrargh.com>
2012-01-28 01:57:16 +1100

    RSS no Tests as yet

---
+++ src/Ideastrike.Nancy/Helpers/FeedHelpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using Nancy;
+
+namespace Ideastrike.Nancy.Helpers
+{
+    public class FeedHelpers
+    {
+        public class FeedResponse<TModel> : Response
+        {
+            public FeedResponse(TModel model)
+            {
+                this.Contents = GetXmlContents(model);
+                this.ContentType = "application/atom+xml";
+                this.StatusCode = HttpStatusCode.OK;
+            }
+
+            private static Action<Stream> GetXmlContents(TModel model)
+            {
+                var m = model as Atom10FeedFormatter;
+
+                return stream =>
+                {
+                    using (XmlWriter writer = XmlWriter.Create(stream))
+                    {
+                        m.WriteTo(writer);
+                    }
+                };
+            }
+        }
+    }
+
+    public static class FormatterExtentions
+    {
+        public static Response AsFeed<TModel>(this IResponseFormatter formatter, TModel model)
+        {
+            return new FeedHelpers.FeedResponse<TModel>(model);
+        }
+    }
+
+}
---
+++ src/Ideastrike.Nancy/Modules/FeedModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using Ideastrike.Nancy.Helpers;
+using Ideastrike.Nancy.Models.Repositories;
+using Nancy;
+
+namespace Ideastrike.Nancy.Modules
+{
+    public class FeedModule : NancyModule
+    {
+        private readonly IIdeaRepository _ideas;
+
+        public FeedModule(IIdeaRepository ideas)
+        {
+            _ideas = ideas;
+            Get["/feed"] = _ =>
+                               {
+                                   var ideasResults = _ideas.GetAll().OrderByDescending(i => i.Time).Take(50);
+
+                                   var items = new List<SyndicationItem>();
+
+                                   string host = UrlExtensions.ToPublicUrl(HttpContext.Current, new Uri("", UriKind.Relative));
+
+                                   foreach (var idea in ideasResults)
+                                   {
+                                       var url = new Uri(string.Format("{0}{1}", host, string.Format("ideas/{0}", idea.Id)));
+                                       items.Add(new SyndicationItem(idea.Title, idea.Description, url, idea.Id.ToString(), idea.Time));
+                                   }
+
+                                   var feed = new Atom10FeedFormatter(new SyndicationFeed("Code52 Ideastrike", "", new Uri(string.Format("{0}{1}", host, "feed")), items)
+                                               {
+                                                   Id = string.Format("{0}{1}", host, "feed"),
+                                                   Links =
+                                                       {
+                                                           new SyndicationLink(new Uri(string.Format("{0}{1}", host, "feed")))
+                                                               {
+                                                                   RelationshipType = "self"
+                                                               }
+                                                       },
+                                                   LastUpdatedTime = DateTime.Now
+                                               });
+
+                                   return Response.AsFeed(feed);
+                               };
+        }
+    }
+}
+
