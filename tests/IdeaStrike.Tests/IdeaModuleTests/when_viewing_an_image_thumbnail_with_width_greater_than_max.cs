29848552269597d971e63d71b75758813b49ee7f
Ian Oxley <ijoxley@gmail.com>
2012-02-07 13:47:14 +0000

    Added unit tests for viewing image thumbnails and setting the max thumbnail width
    The viewing image thumbnails unit tests check that a width parameter
    greater than the max doesn't cause an error, and generates a HTTP status
    code of 200.
    
    The admin settings unit tests check that the form field to set the max
    thumbnail width is included in the response.

---
+++ tests/IdeaStrike.Tests/IdeaModuleTests/when_viewing_an_image_thumbnail_with_width_greater_than_max.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ideastrike.Nancy.Modules;
+using Ideastrike.Nancy.Models;
+using Xunit;
+
+namespace IdeaStrike.Tests.IdeaModuleTests
+{
+    public class when_viewing_an_image_thumbnail_with_width_greater_than_max : IdeaStrikeSpecBase<IdeaModule>
+    {
+        private Image testImage;
+
+        public when_viewing_an_image_thumbnail_with_width_greater_than_max()
+        {
+            testImage = new Image
+            {
+                Id = 1,
+                Name = "Test Image",
+                ImageBits = CreateImageBits(),
+                IdeaId = 1
+            };
+            _Images.Setup(i => i.Get(1)).Returns(testImage);
+            _Settings.SetupGet(s => s.MaxThumbnailWidth).Returns(1000);
+            Get("/idea/imagethumb/1/5000000");
+        }
+
+        [Fact]
+        public void it_should_return_status_ok()
+        {
+            Assert.Equal(Nancy.HttpStatusCode.OK, Response.StatusCode);
+        }
+    }
+}
