a08ad539cda4e41a41c26d2b29dc87e896b30dbd
alexander.beletsky <alexander.beletsky@gmail.com>
2012-02-10 15:53:09 +0200

    issue #83 highlighting of invalid fields

--- src/Ideastrike.Nancy/Resources/Validation/newidea.js
+++ src/Ideastrike.Nancy/Resources/Validation/newidea.js
@@ -2,19 +2,28 @@
     $('form').submit(function () {
         var invalid = false;
 
-        invalid = $('#title').val() == '';
-        invalid = invalid || $('#new-idea').val() == '';
+        if ($('#title').val() == '') {
+            invalid = true;
+            $('#title').closest('.clearfix').addClass('error');
+        }
+
+        if ($('#new-idea').val() == '') {
+            invalid = true;
+            $('#new-idea').closest('.clearfix').addClass('error');
+        }
 
         if (invalid) {
-            $('.error').show();
+            $('.alert-message').show();
         }
 
         return !invalid;
     });
 
     $('#title, #new-idea').live('keyup', function () {
-        if ($('#title').val().length > 0 && $('#new-idea').val().length > 0) {
-            $('.error').hide();
+        if ($(this).val().length > 0) {
+            $(this).closest('.clearfix').removeClass('error');
+            $('.alert-message').hide();
         }
     });
+
 })();
