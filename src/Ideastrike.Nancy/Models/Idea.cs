986139025b76248ff82f1e75550a3f26a9241ab5
alexander.beletsky <alexander.beletsky@gmail.com>
2012-02-10 10:22:37 +0200

    fixed some code warnings

--- src/Ideastrike.Nancy/Models/Idea.cs
+++ src/Ideastrike.Nancy/Models/Idea.cs
@@ -17,7 +17,9 @@ namespace Ideastrike.Nancy.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         public string Status { get; set; }
         public DateTime Time { get; set; }
