dca30966a36c3ba74a95298ab09a591122fd8911
Mikael Koskinen <mijapkos@gmail.com>
2012-02-15 21:24:36 +0200

    Added support for localization. Moved strings to resource file, added Finnish localization

--- src/Ideastrike.Nancy/Modules/IdeaModule.Secured.cs
+++ src/Ideastrike.Nancy/Modules/IdeaModule.Secured.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Ideastrike.Nancy.Helpers;
+using Ideastrike.Nancy.Localization;
 using Ideastrike.Nancy.Models;
 using Ideastrike.Nancy.Models.Repositories;
 using Nancy;
@@ -51,7 +52,7 @@ namespace Ideastrike.Nancy.Modules
                 if (idea.Author.UserName != Context.CurrentUser.UserName)
                     return View["404"];
 
-                var m = Context.Model(string.Format("Edit Idea: '{0}' - {1}", idea.Title, _settings.Title));
+                var m = Context.Model(string.Format(Strings.IdeaSecuredModule_EditIdea, idea.Title, _settings.Title));
                 m.PopularIdeas = _ideas.GetAll();
                 m.Idea = idea;
                 m.StatusChoices = _settings.IdeaStatusChoices.Split(',');
