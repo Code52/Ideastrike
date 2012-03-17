6893d441ecf33387e935fd6614a38407aad42527
Brendan Forster <me@brendanforster.com>
2012-03-17 14:52:13 +1100

    fixed tests, moved build out of csproj and into build script

--- tests/IdeaStrike.Tests/FeatureModuleTests/when_an_unauthenticated_user_adds_a_features.cs
+++ tests/IdeaStrike.Tests/FeatureModuleTests/when_an_unauthenticated_user_adds_a_features.cs
@@ -1,22 +1,21 @@
-﻿using Ideastrike.Nancy.Models;
-using Ideastrike.Nancy.Modules;
-using Moq;
-using Nancy;
+﻿using Ideastrike.Nancy.Modules;
 using Nancy.Testing;
 using Xunit;
 
 namespace IdeaStrike.Tests.FeatureModuleTests
 {
-	public class when_an_unauthenticated_user_adds_a_features : IdeaStrikeSpecBase<FeatureModule>
-	{
-		public when_an_unauthenticated_user_adds_a_features() {
-			EnableFormsAuth();
-			Post("/idea/0/feature");
-		}
+    public class when_an_unauthenticated_user_adds_a_features : IdeaStrikeSpecBase<FeatureModule>
+    {
+        public when_an_unauthenticated_user_adds_a_features()
+        {
+            EnableFormsAuth();
+            Post("/idea/0/feature");
+        }
 
-		[Fact]
-		public void it_should_redirect_to_the_login_page() {
-			Response.ShouldHaveRedirectedTo("/login");
-		}
-	}
+        [Fact]
+        public void it_should_redirect_to_the_login_page()
+        {
+            Response.ShouldHaveRedirectedTo("/login");
+        }
+    }
 }
--- tests/IdeaStrike.Tests/HomeModuleTests/when_viewing_the_home_page.cs
+++ tests/IdeaStrike.Tests/HomeModuleTests/when_viewing_the_home_page.cs
@@ -1,19 +1,22 @@
 ﻿using Ideastrike.Nancy.Modules;
 using Nancy;
-using Nancy.Testing;
+using Nancy.Responses;
 using Xunit;
 
 namespace IdeaStrike.Tests.HomeModuleTests
 {
-	public class when_viewing_the_home_page : IdeaStrikeSpecBase<HomeModule>
-	{
-		public when_viewing_the_home_page() {
-			Get("/");
-		}
+    public class when_viewing_the_home_page : IdeaStrikeSpecBase<HomeModule>
+    {
+        public when_viewing_the_home_page()
+        {
+            SetView("Home/Index", new HtmlResponse());
+            Get("/");
+        }
 
-		[Fact]
-		public void it_should_set_the_status_code_to_ok_for_the_home_page() {
-			Assert.Equal(HttpStatusCode.OK, Response.StatusCode);
-		}
-	}
+        [Fact]
+        public void it_should_set_the_status_code_to_ok_for_the_home_page()
+        {
+            Assert.Equal(HttpStatusCode.OK, Response.StatusCode);
+        }
+    }
 }
