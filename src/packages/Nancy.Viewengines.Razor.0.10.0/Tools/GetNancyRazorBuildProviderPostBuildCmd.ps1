8a39f71930a0f9ed7a187a654bd5cd69748b6332
Brendan Forster <me@brendanforster.com>
2012-03-24 14:19:11 +1100

    removed old package to ensure it isn't an upstream issue

--- src/packages/Nancy.Viewengines.Razor.0.10.0/Tools/GetNancyRazorBuildProviderPostBuildCmd.ps1
+++
@@ -1,9 +0,0 @@
-$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
-$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
-
-$BuildProvidersDir = Join-Path $path "BuildProviders"
-$ViewEngineDir = Join-Path $path "lib\Net40"
-
-$NancyRazorBuildProviderPostBuildCmd = "
-start /MIN xcopy /s /y /R `"$BuildProvidersDir\Nancy.ViewEngines.Razor.BuildProviders.dll`" `"`$(ProjectDir)bin`"
-start /MIN xcopy /s /y /R `"$ViewEngineDir\Nancy.ViewEngines.Razor.dll`" `"`$(ProjectDir)bin`""
